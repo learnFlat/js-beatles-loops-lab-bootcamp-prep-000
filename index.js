c8c34eddaed6a68da233b07190e8f1e1891b0440
C Z <xzcynthiazheng@gmail.com>
2018-08-05 15:22:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay(musi, inst) {
+  var arr = []
+  for() {
+    
+  }
+}
+
+function iLoveTheBeatles(num) {
+  var arr = []
+  do {
+    arr.push('I love the Beatles!')
+    
+  } while (num < 15) {
+    num++
+  }
+  return arr
+}
